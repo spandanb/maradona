7bcb791ce4c6a1c89026c1670fdf87f182481a2c
Spandan Bemby <spandan.b@myplanetdigital.com>
2012-08-30 18:50:07 -0400

    first commit, new rails app created

---
+++ maradona/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maradona::Application.config.secret_token = '357c809f383affbee3eb80a54c34ecbcd3f98584b44b289b63ea5d262dd00095e6eedcec67a2e3c5557c76b509c71857dcceca92083e40231ae65df325c27500'
