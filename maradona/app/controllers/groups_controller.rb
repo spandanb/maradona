6b33e0b762cf361b00adfd817f28ed12c673755c
Wasi Ahmed <wasi.cjr@gmail.com>
2012-09-09 18:42:02 -0400

    Added Posts to Groups

--- maradona/app/controllers/groups_controller.rb
+++ maradona/app/controllers/groups_controller.rb
@@ -5,8 +5,16 @@ class GroupsController < ApplicationController
 	end	
 	
 	def show
-    @group = Group.find(params[:id])    
-  end
+    	@group = Group.find(params[:id])    
+	@user = current_user
+	@post = current_user.posts.build if logged_in?
+	@post.user_id = current_user.id
+	@post.group_id = @group.id.to_i
+
+
+	@posts = Post.find_by_sql("SELECT \"posts\".* FROM \"posts\" WHERE \"posts\".\"group_id\" = #{@group.id} ORDER BY posts.created_at DESC")
+
+	end
   
 	def new
 		#By default, the current_user is added to the group
