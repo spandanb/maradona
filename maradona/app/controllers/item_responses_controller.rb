798387ce87ba12aedac27f92ee5a1194eb1d2b31
Spandan Bemby <spandan.b@myplanetdigital.com>
2012-09-30 21:27:42 -0400

    input loading via ajax

--- maradona/app/controllers/item_responses_controller.rb
+++ maradona/app/controllers/item_responses_controller.rb
@@ -2,8 +2,20 @@ class ItemResponsesController < ApplicationController
 
 	def create
 		@item_response = ItemResponse.new(params[:item_response])
+=begin				
+		respond_to do |format|
+      if @item_response.save
+        format.js { render }
+      end
+    end
+=end		
 		@item_response.save
 		redirect_to :back
 	end
 		
+	def new 	
+		@item = Item.find(params[:item_id]) #seems hacky
+		
+	end
+
 end
